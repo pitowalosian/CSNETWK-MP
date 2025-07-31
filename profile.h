d897dcf0f2bf05e7d5d472c47dca5dafd3f0847a
pitowalosian <sian_obregon@dlsu.edu.ph>
2025-07-31 10:55:20 +0800

    Create profile.h

---
+++ profile.h
@@ -0,0 +1,25 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#define MAXSTRING 256
+#define CONSTANT 50
+#define MAX 20480
+
+typedef struct {
+	char type[CONSTANT];
+	char userID[MAXSTRING];
+	char displayName[CONSTANT];
+	char status[MAXSTRING];
+	char avatarType[CONSTANT];
+	char avatarEncoding[CONSTANT];
+	char avatarData[MAX];
+	int hasAvatar;
+} Profile;
+
+Profile createProfile(const char* userID, const char* displayName, const char* status,
+		const char* avatarType, const char* avatarEncoding, const char* avatarData);
+		
+void printVerboseProfile(Profile p);
+void printSimpleProfile(Profile p);
+
+#endif // PROFILE_H
